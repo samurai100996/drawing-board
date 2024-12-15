63691e2302c1b76a132cd5f45b01233f5a9fd6e7
samurai100996 <sauravshelke2003@gmail.com>
2024-12-15 19:03:33 +0530

    lets go some changes

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
-const boxx = document.querySelectorAll(".box");
+// const boxx = document.querySelectorAll(".box");
 
-for(let i =0;i<=2500;i++){
-    boxx[i].addEventListener("mouseover",()=>{
-        boxx[i].style.backgroundColor ="red";
-    });
+// for(let i =0;i<=2500;i++){
+//     boxx[i].addEventListener("mouseover",()=>{
+//         boxx[i].style.backgroundColor ="red";
+//     });
+// }
+const boxes = document.querySelectorAll(".box"); // [,,,,,,.....]
+const colorInput = document.getElementById("color-input");
+
+let canDraw = false;
+let color = "black";
+
+document.addEventListener("mousedown", () => {
+  canDraw = true;
+});
+
+document.addEventListener("mouseup", () => {
+  canDraw = false;
+});
+
+for (let i = 0; i < 2500; i++) {
+  boxes[i].addEventListener("mouseover", () => {
+    if (canDraw) {
+      boxes[i].style.backgroundColor = color;
+    }
+  });
+}
+
+colorInput.addEventListener("change", () => {
+  color = colorInput.value;
+});
+
+function erase() {
+  color = "white";
 }
